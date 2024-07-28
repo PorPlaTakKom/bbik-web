609364105d7c665a3e9d2b49dab51bb810871627
KangPla <kangpla@MacBook-Air.local>
2024-07-28 18:47:53 +0700

    [FIX] ENV

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_API': JSON.stringify(process.env.VITE_API),
+  },
   plugins: [react()],
 })
