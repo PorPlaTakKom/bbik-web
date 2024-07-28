871c725e1070e9f3b1bc0e2402e6784d5ddd1893
KangPla <kangpla@MacBook-Air.local>
2024-07-28 17:26:21 +0700

    [UPDATE] web

--- src/services/todoService.jsx
+++ src/services/todoService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1/todos';
+const API_URL = `http://${import.meta.env.VITE_API}:8080/api/v1/todos`;
 
 export const getTodos = async () => {
     try {
